ed37bd48053239e9c8f98a9fe9904886db8b3717
CardenasHL <126452686+CardenasHL@users.noreply.github.com>
2024-03-04 09:26:12 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,13 @@
-sendEmailToUnity('usuario@example.com');
+// Definir la función para llamar a sendEmailToUnity
+function callSendEmailToUnity() {
+    var email = "usuario@example.com";
+    sendEmailToUnity(email);
+}
+
+// Llama a sendEmailToUnity desde el script externo
+if (sceneLoaded) {
+    callSendEmailToUnity();
+} else {
+    // Si Unity aún no está listo, espera a que lo esté
+    unityReadyCallbacks.push(callSendEmailToUnity);
+}
