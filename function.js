281161fffcfbde9c7ffad576426da3a83ef17b3f
CardenasHL <126452686+CardenasHL@users.noreply.github.com>
2024-03-04 09:56:11 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,11 +3,20 @@ function callSendEmailToUnity() {
     var email = "usuario@example.com";
     sendEmailToUnity(email);
 }
-
-// Llama a sendEmailToUnity desde el script externo
-if (sceneLoaded) {
+// Verificar si la variable sceneLoaded existe y es verdadera
+if (typeof sceneLoaded !== 'undefined' && sceneLoaded) {
+    // La variable existe y es verdadera
     callSendEmailToUnity();
 } else {
-    // Si Unity aún no está listo, espera a que lo esté
-    unityReadyCallbacks.push(callSendEmailToUnity);
+    // La variable no existe o es falsa
+    console.error("Unity aún no está listo. Esperando a que esté listo...");
+
+    // Verificar si la variable existe
+    if (typeof sceneLoaded !== 'undefined') {
+        // La variable existe pero es falsa, agregar a unityReadyCallbacks
+        unityReadyCallbacks.push(callSendEmailToUnity);
+    } else {
+        // La variable no existe, imprime un mensaje en la consola
+        console.error("La variable sceneLoaded no está definida.");
+    }
 }
